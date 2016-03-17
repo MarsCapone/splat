742c2fa8fb4437f86ef38a37ad00c81bd52cb543
Oliver Steptoe <os4g14@soton.ac.uk>
2016-03-17 00:20:36 +0000

    Add task comments and reduced output crap

--- src/main.ml
+++ src/main.ml
@@ -9,15 +9,12 @@ let parseProgram c =
             parser_main lexer_main lexbuf
     with Parsing.Parse_error -> failwith "Parse failure!" ;;
 
-Parsing.set_trace true;
+(* Parsing.set_trace true; *)
 let arg = ref stdin in
 let setProg p = arg := open_in p in
 let usage = "./main PROGRAM_FILE" in
 parse [] setProg usage ;
 let parsedProg = parseProgram !arg in
-let () = print_string "Program Parsed" ; print_newline() in
-let _ = typeProg parsedProg in
-let () = print_string "Program Type Checked" ; print_newline() in
 let result1 = evalProg parsedProg in
-let () = print_string "Program Evaluated to ==> " ; print_res result1 ; print_newline() in
+let () = print_string "Returned " ; print_res result1 ; print_newline() in
 flush stdout
